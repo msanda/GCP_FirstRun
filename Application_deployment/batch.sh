b35c895803c82c7b09b23f1c085862563b613795
msanda <msanda77@hotmail.com>
2018-04-19 21:12:06 -0400

    batch.sh
    test case

---
+++ Application_deployment/batch.sh
@@ -0,0 +1,12 @@
+apt-get install python-pip python-dev -y
+mkdir /var/www/app
+cd /var/www/app
+wget https://raw.githubusercontent.com/GoogleCloudPlatform/python-docs-samples/master/appengine/standard/cloudsql/main.py
+wget https://raw.githubusercontent.com/GoogleCloudPlatform/python-docs-samples/master/appengine/standard/cloudsql/main_test.py
+pip install uwsgi 
+sudo echo 'CLOUDSQL_CONNECTION_NAME=abletogcpproject:us-east1:abletogc-database' >> /etc/environment
+sudo echo 'CLOUDSQL_USER=root' >> /etc/environment
+sudo echo 'CLOUDSQL_PASSWORD=simpletest' >> /etc/environment
+export CLOUDSQL_CONNECTION_NAME=abletogcpproject:us-east1:abletogc-database
+export CLOUDSQL_USER=root
+export CLOUDSQL_PASSWORD=simpletest
