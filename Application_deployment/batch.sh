7ca1f34b971335214fcc82470039f03cdb2a418c
msanda <msanda77@hotmail.com>
2018-04-19 22:52:25 -0400

    Update batch.sh

--- Application_deployment/batch.sh
+++ Application_deployment/batch.sh
@@ -5,9 +5,16 @@ cd /var/www/app
 wget https://raw.githubusercontent.com/GoogleCloudPlatform/python-docs-samples/master/appengine/standard/cloudsql/main.py
 wget https://raw.githubusercontent.com/GoogleCloudPlatform/python-docs-samples/master/appengine/standard/cloudsql/main_test.py
 sudo pip install uwsgi 
+sudo pip install WebOb
+sudo pip install Paste
+sudo pip install webapp2
 sudo echo 'CLOUDSQL_CONNECTION_NAME=abletogcpproject:us-east1:abletogc-database' >> /etc/environment
 sudo echo 'CLOUDSQL_USER=root' >> /etc/environment
 sudo echo 'CLOUDSQL_PASSWORD=simpletest' >> /etc/environment
 export CLOUDSQL_CONNECTION_NAME=abletogcpproject:us-east1:abletogc-database
 export CLOUDSQL_USER=root
 export CLOUDSQL_PASSWORD=simpletest
+
+./cloud_sql_proxy -instances=abletogcpproject:us-east1:abletogc-database=tcp:3306 &
+
+sudo service nginx restart
